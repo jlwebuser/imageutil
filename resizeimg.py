df86f3099c93258f781851b770331a143f7dc2e3
John Love <john.love@gmail.com>
2018-12-09 23:11:29 -0800

    lint and argparse cleanup

--- resizeimg.py
+++ resizeimg.py
@@ -16,7 +16,7 @@ def parse_args():
     '''Process command line'''
     description = '''resize images in specified folders such that if thier width
 is > --width it will reduced to width preserving aspect ratio'''
-    parser = argparse.ArgumentParser(description)
+    parser = argparse.ArgumentParser(description, usage="resizeimg.py --width 250 dir1 dir2...")
     parser.add_argument('--width', dest='width', required=True, type=int,
                         help='width in pixels to clamp image size to')
     parser.add_argument('args', nargs=argparse.REMAINDER,
@@ -33,28 +33,29 @@ def fixsize(filename, width):
         r = width / float(w)
         dim = (width, int(h * r))
         img = cv2.resize(img, dim)
-        print("resized:", filename, "from", (w, h), "to", dim)
+        print("resized:", filename, "from", (w, h), "to", dim, file=sys.stderr)
         if not cv2.imwrite(filename, img):
-            print("failed writing:", filename)
+            print("failed writing:", filename, file=sys.stderr)
             return None
     else:
-        print("No change", filename, (w, h))
+        print("No change", filename, (w, h), file=sys.stderr)
         dim = (w, h)
 
     return dim
 
 ARGS = parse_args()
-if ARGS.width > 2000:
-    print("--width should be less than 2000")
+if not ARGS.args:
+    print(sys.argv[0] + ": no directory specified", file=sys.stderr)
     sys.exit()
-if ARGS.args < 1:
-    print("no directories to process")
+
+if ARGS.width > 2000:
+    print("--width should be less than 2000", file=sys.stderr)
     sys.exit()
 
 for fdir in ARGS.args:
     files = [os.path.join(fdir, x) for x in fnmatch.filter(os.listdir(fdir), '*.jpg')]
-    if files:
-        print("skipping: no jpg files in", fdir)
+    if not files:
+        print("skipping: no jpg files in", fdir, file=sys.stderr)
         continue
     for f in files:
         fixsize(f, ARGS.width)
