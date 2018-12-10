f4f61170438bc4849e4be0bf0a42f20e8dfb5cf1
John Love <john.love@gmail.com>
2018-12-09 22:02:22 -0800

    change to use argparse, lint fixes

--- makelst.py
+++ makelst.py
@@ -23,23 +23,34 @@ large (like 500 in the hyperparameters))
 import sys
 import os
 import fnmatch
+import argparse
 import cv2
+
+def parse_args():
+    '''Load args...'''
+    description = '''Generate an LST format to standard out from JPG files and
+    label files in the yolo format for processing by im2rec.py\n'''
+    parser = argparse.ArgumentParser(description)
+    parser.add_argument('--imagedir', dest='imagedir', required=True,
+                        help='process jpg/txt files in imagedir')
+    args = parser.parse_args()
+    return args
 
 def processfile(srcjpg):
     '''
     Read the jpg file specified
-    Read its associated lable (.txt) file that with the same directory/basename
-    ending with .txt
+    Read its associated label (.txt) file from the same directory/basename
+    ending with .txt (if it exists) If the txt file does not exist, or has
+    no labels then a single label of [-1,-1,-1,-1,-1] will be emitted.
     Produce the w, h, and a list containing the 0..n class id, and convert the
     center/extents of the label to a bounding rectangle
     Return None if it can't read the jpg file'''
     img = cv2.imread(srcjpg)
     if img is None:
-        print(sys.argv[0], "Skipping image: failed reading", srcjpg)
+        print(sys.argv[0], "Skipping image: failed reading", srcjpg, file=sys.stderr)
         return None
 
-    height, width, channels = img.shape
-
+    height, width, _ = img.shape
     txtfile = srcjpg[:-4] + ".txt"
     try:
         # if it exists, process the label box dimensions
@@ -56,54 +67,42 @@ def processfile(srcjpg):
                 ymax = round(float(s[2]) + float(s[4])/2, 5)
                 labels.append([str(float(s[0])), str(xmin), str(ymin), str(xmax), str(ymax)])
     except IOError:
-        it = 1
-    if labels is None:
+        pass
+    if not labels:
+        # No labels for this image, but im2rec expects at least one, so pass this
         labels.append(["-1.", "-1.", "-1.", "-1.", "-1."])
     return True, width, height, labels
 
 #
 #  Main section that plucks and validates the args, and processes the file list
 #
-count = 0
-if len(sys.argv) == 2:
-    # get path to directory to convert
-    path = sys.argv[1]
-    # validate the dir path and scan for jpg files
-    try:
-        dirlist = fnmatch.filter(os.listdir(path), "*.jpg")
-    except:
-        print(sys.argv[0], ": exception accessing", sys.argv[1])
-        sys.exit()
+ARGS = parse_args()
+COUNT = 0
+# validate the dir path and scan for jpg files
+try:
+    DIRLIST = fnmatch.filter(os.listdir(ARGS.imagedir), "*.jpg")
+except os.error:
+    print(sys.argv[0], ": exception accessing", ARGS.imagedir, file=sys.stderr)
+    sys.exit()
 
-    # get the lst file name to be created
-    #lstfile = sys.argv[2]
-    # process eash file in the dirlist
-    for file in dirlist:
-        # form the full path to the source jpg file, read it and get the dimensions
-        srcjpg = os.path.join(path, file)
-        success, width, height, labels = processfile(srcjpg)
-        labels = [item for sublist in labels for item in sublist]
+# process eash file in the dirlist
+for file in DIRLIST:
+    # form the full path to the source jpg file, read it and get the dimensions
+    src_jpg = os.path.join(ARGS.imagedir, file)
+    success, _, _, labels = processfile(src_jpg)
+    label_s = [item for sublist in labels for item in sublist]
 
-        # this line format is documented https://mxnet.incubator.apache.org/api/python/image/image.html#image-iterator-for-object-detection
-        # Index, A  B  [extra header]  [(object0), (object1), ... (objectN)] file
-        # Where A is the width (number of fields including itself) of header (2 which are A(this number), and B (the number of fields in a single label)),
-        # B is the number of fields in a label. for this program a label has 5 fields  classid, xmin, ymin, xmax, ymax
-        # if you wanted the image width and height in the header, just bump the A (str(2)) value up to str(4), and put str(width), str(height) before B(str(5))
-        # i've not found anything that required width/height so leaving it out.
-        lstline = [str(count), str(2), str(5) ] + labels + [file]
-        print ('\t'.join(lstline))
-        #flat_list = [item for sublist in labels for item in sublist]
-        if success:
-            count +=1
-
-else:
-    print("usage:", sys.argv[0], "directory-containing-jpg")
-
-
-
-
-
-
-
-
-
+    # this line format is documented https://mxnet.incubator.apache.org/api/python/
+    # image/image.html#image-iterator-for-object-detection
+    # Index, A  B  [extra header]  [(object0), (object1), ... (objectN)] file
+    # Where A is the width (number of fields including itself) of header (2 which
+    # are A(this number), and B (the number of fields in a single label)),
+    # B is the number of fields in a label. for this program a label has 5 fields
+    # classid, xmin, ymin, xmax, ymax
+    # if you wanted the image width and height in the header, just bump the
+    # A (str(2)) value up to str(4), and put str(width), str(height) before B(str(5))
+    # i've not found anything that required width/height so leaving it out.
+    lstline = [str(COUNT), str(2), str(5)] + label_s + [file]
+    print('\t'.join(lstline))
+    if success:
+        COUNT += 1
